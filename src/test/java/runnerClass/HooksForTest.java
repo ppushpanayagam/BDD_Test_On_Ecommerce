e149e8e0a1411487ff4bb7b0e96e26c2052c6cca
Pushparaj Pushpanayagam <ppushpanayagam@gmail.com>
2020-10-20 17:28:13 +0100

    Added wrapper class and hooks to set up the framework

---
+++ src/test/java/runnerClass/HooksForTest.java
@@ -0,0 +1,17 @@
+package runnerClass;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class HooksForTest {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+}
